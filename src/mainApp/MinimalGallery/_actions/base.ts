65dfbd72dd34add49b2e51241a6ad23a66818a82
Nick <nsenger@esri.com>
2017-11-15 14:40:29 -0800

    feat: Adds support for >100 items in minimal gallery

--- src/mainApp/MinimalGallery/_actions/base.ts
+++ src/mainApp/MinimalGallery/_actions/base.ts
@@ -4,6 +4,7 @@ export const LOAD_APP_PROGRESS = "LOAD_APP_PROGRESS";
 export const LOAD_APP_SUCCESS = "LOAD_APP_FINISH";
 
 import * as all from "dojo/promise/all";
+import * as Deferred from "dojo/Deferred";
 import { updateItems, push, hashChange } from ".";
 import { MinimalGalleryState } from "..";
 import supportedItemTypes from "../_utilities/supportedItemTypes";
@@ -26,15 +27,7 @@ const queryGroupItems = (applicationBaseResult: __esriApplicationBase.Applicatio
         const state = getState();
         const applicationBase = state.base.applicationBase;
         const config = applicationBaseResult.config;
-        applicationBase.queryGroupItems(
-            config.group,
-            {
-                num: 100,
-                sortField: (config.sortField ? config.sortField : "num-views"),
-                sortOrder: (config.sortOrder ? config.sortOrder : "desc"),
-                start: 0
-            } as __esri.PortalQueryParams
-        ).then(
+        fetchAllGroupItems(applicationBase, config).then(
             (response: any) => {
                 const promises = response.results.map((item: any) => item.load());
                 all(promises).then(
@@ -51,6 +44,50 @@ const queryGroupItems = (applicationBaseResult: __esriApplicationBase.Applicatio
             (err: any) => dispatch(loadAppFail(err))
         );
     };
+
+const fetchAllGroupItems = (applicationBase: MinimalGalleryState["base"]["applicationBase"], config: any) => {
+    const dfd = new Deferred;
+
+    applicationBase.queryGroupItems(
+        config.group,
+        {
+            num: 100,
+            sortField: (config.sortField ? config.sortField : "numviews"),
+            sortOrder: (config.sortOtder ? config.sortOrder : "desc"),
+            start: 0
+        } as __esri.PortalQueryParams
+    ).then((response: any) => {
+        if (response.total > response.results.length) { // This group is more than 100 items, fetch them all!
+            const mappableArr = Array.apply(null, { length: Math.floor(response.total / 100) });
+            const promises = mappableArr.map((c: any, i: number) => (
+                applicationBase.queryGroupItems(
+                    config.group,
+                    {
+                        num: 100,
+                        sortField: (config.sortField ? config.sortField : "numviews"),
+                        sortOrder: (config.sortOtder ? config.sortOrder : "desc"),
+                        start:  100 + (i * 100)
+                    } as __esri.PortalQueryParams
+                )
+            ));
+            all(promises).then((responses: any) => {
+                const allItems = responses.reduce((p: any, c: any) => {
+                    p.results = p.results.concat(c.results);
+                    return p;
+                }, { results: response.results });
+                dfd.resolve(allItems);
+            }, (err: Error) => {
+                dfd.reject(err);
+            });
+        } else {
+            dfd.resolve(response);
+        }
+    }, (err: Error) => {
+        dfd.reject(err);
+    });
+
+    return dfd;
+};
 
 const saveAppBaseResult = (applicationBaseResult: __esriApplicationBase.ApplicationConfig) => ({
     type: SAVE_APP_BASE_RESULT,
