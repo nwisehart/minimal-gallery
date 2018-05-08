aac12beaffbe6e015f109ee67ed0ceee405da54b
Kelly Hutchins <khutchins@esri.com>
2018-05-08 11:24:55 -0700

    added nls files for new locales

---
+++ dist/config/nls/hu/resources.js
@@ -0,0 +1,76 @@
+﻿define({
+    root: ({
+      ui: {
+        "close": "Bezárás",
+        "mapExtTip": "Teljes képernyős térkép megnyitása",
+        "sceneExtTip": "Teljes képernyős 3D térkép megnyitása",
+        "appExtTip": "Teljes képernyős alkalmazás megnyitása",
+        "fileExtTip": "Fájl letöltése",
+        "itemExtTip": "ArcGIS Online elemoldal megnyitása",
+        "galleryTip": "Megnyitás a Galériában"
+      },
+      appLoading: {
+        "init": "Alkalmazás inicializálása...",
+        "login": "Felhasználói bejelentkezés ellenőrzése...",
+        "portalapp": "Portál és alkalmazás lekérdezése...",
+        "groupdata": "Csoportadatok lekérdezése...",
+        "groupprocess": "Csoportválasz feldolgozása...",
+        "groupitems": "Elemek betöltése...",
+        "failed": "Nem sikerült betölteni az alkalmazást."
+      },
+      viewLoading: {
+        "scripts": "Parancsfájlok lekérése...",
+        "map": "Térkép előkészítése...",
+        "basemap": "Alaptérkép betöltése...",
+        "layers": "Rétegek feldolgozása...",
+        "view": "Nézet inicializálása...",
+        "widgets": "Widgetek hozzáadása...",
+        "failed": "Nem sikerült betölteni az elemet.",
+        "sorry": "Sajnáljuk, hiba történt a kért elem betöltésekor.",
+        "iframe": "Sajnáljuk, a böngésző nem rendelkezik iframe-támogatással."
+      },
+      header: {
+        "agol": "Megtekintés az ArcGIS Online webhelyen",
+        "search": "Keresés"
+      },
+      pager: {
+        "noResults": "A keresés nem adott vissza eredményt."
+      }
+    }),
+    "ar": 1,
+    "bs": 1,
+    "cs": 1,
+    "da": 1,
+    "de": 1,
+    "el": 1,
+    "es": 1,
+    "et": 1,
+    "fi": 1,
+    "fr": 1,
+    "he": 1,
+    "hi": 1,
+    "hr": 1,
+    "id": 1,
+    "it": 1,
+    "ja": 1,
+    "ko": 1,
+    "lt": 1,
+    "lv": 1,
+    "nl": 1,
+    "nb": 1,
+    "pl": 1,
+    "pt-br": 1,
+    "pt-pt": 1,
+    "ro": 1,
+    "ru": 1,
+    "sl": 1,
+    "sr": 1,
+    "sv": 1,
+    "th": 1,
+    "tr": 1,
+    "vi": 1,
+    "zh-cn": 1,
+    "zh-hk": 1,
+    "zh-tw": 1
+  });
+  
