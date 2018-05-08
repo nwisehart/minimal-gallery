6c93b1a3d734edf605976b451a68d7dc6a6129a9
Jonathan Turpin <jturpin@esri.com>
2018-05-08 14:30:54 -0700

    Updated Pseudo i18n - @jona7150`

--- dist/config/nls/ca/resources.js
+++ dist/config/nls/ca/resources.js
@@ -1,76 +1,38 @@
-﻿define({
-    root: ({
-      ui: {
-        "close": "Tanca",
-        "mapExtTip": "Obre el mapa de pantalla completa",
-        "sceneExtTip": "Obre l\'escena de pantalla completa",
-        "appExtTip": "Obre l\'aplicació de pantalla completa",
-        "fileExtTip": "Baixa el fitxer",
-        "itemExtTip": "Obre la pàgina de l\'element de l\'ArcGIS Online",
-        "galleryTip": "Obre-ho a la galeria"
-      },
-      appLoading: {
-        "init": "S\'està inicialitzant l\'aplicació...",
-        "login": "S\'està comprovant l\'inici de sessió de l\'usuari...",
-        "portalapp": "S\'estan consultant el portal i l\'aplicació...",
-        "groupdata": "S\'estan consultant les dades del grup...",
-        "groupprocess": "S\'està processant la resposta del grup...",
-        "groupitems": "S\'estan carregant els elements...",
-        "failed": "No s\'ha pogut carregar l\'aplicació."
-      },
-      viewLoading: {
-        "scripts": "S\'estan recuperant els scripts...",
-        "map": "S\'està preparant el mapa...",
-        "basemap": "S\'està carregant el mapa base...",
-        "layers": "S\'estan processant les capes...",
-        "view": "S\'està inicialitzant la visualització...",
-        "widgets": "S\'estan afegint els widgets...",
-        "failed": "No s\'ha pogut carregar l\'element.",
-        "sorry": "S\'ha produït algun problema en carregar l\'element sol·licitat.",
-        "iframe": "El vostre navegador no admet iframes."
-      },
-      header: {
-        "agol": "Visualitza-ho a l\'ArcGIS Online",
-        "search": "Cerca"
-      },
-      pager: {
-        "noResults": "La cerca no ha retornat cap resultat."
-      }
-    }),
-    "ar": 1,
-    "bs": 1,
-    "cs": 1,
-    "da": 1,
-    "de": 1,
-    "el": 1,
-    "es": 1,
-    "et": 1,
-    "fi": 1,
-    "fr": 1,
-    "he": 1,
-    "hi": 1,
-    "hr": 1,
-    "id": 1,
-    "it": 1,
-    "ja": 1,
-    "ko": 1,
-    "lt": 1,
-    "lv": 1,
-    "nl": 1,
-    "nb": 1,
-    "pl": 1,
-    "pt-br": 1,
-    "pt-pt": 1,
-    "ro": 1,
-    "ru": 1,
-    "sl": 1,
-    "sr": 1,
-    "sv": 1,
-    "th": 1,
-    "tr": 1,
-    "vi": 1,
-    "zh-cn": 1,
-    "zh-hk": 1,
-    "zh-tw": 1
-  });
-  
+define({
+  "ui": {
+    "close": "Tanca",
+    "mapExtTip": "Obre el mapa de pantalla completa",
+    "sceneExtTip": "Obre l'escena de pantalla completa",
+    "appExtTip": "Obre l'aplicació de pantalla completa",
+    "fileExtTip": "Baixa el fitxer",
+    "itemExtTip": "Obre la pàgina de l'element de l'ArcGIS Online",
+    "galleryTip": "Obre-ho a la galeria"
+  },
+  "appLoading": {
+    "init": "S'està inicialitzant l'aplicació...",
+    "login": "S'està comprovant l'inici de sessió de l'usuari...",
+    "portalapp": "S'estan consultant el portal i l'aplicació...",
+    "groupdata": "S'estan consultant les dades del grup...",
+    "groupprocess": "S'està processant la resposta del grup...",
+    "groupitems": "S'estan carregant els elements...",
+    "failed": "No s'ha pogut carregar l'aplicació."
+  },
+  "viewLoading": {
+    "scripts": "S'estan recuperant els scripts...",
+    "map": "S'està preparant el mapa...",
+    "basemap": "S'està carregant el mapa base...",
+    "layers": "S'estan processant les capes...",
+    "view": "S'està inicialitzant la visualització...",
+    "widgets": "S'estan afegint els widgets...",
+    "failed": "No s'ha pogut carregar l'element.",
+    "sorry": "S'ha produït algun problema en carregar l'element sol·licitat.",
+    "iframe": "El vostre navegador no admet iframes."
+  },
+  "header": {
+    "agol": "Visualitza-ho a l'ArcGIS Online",
+    "search": "Cerca"
+  },
+  "pager": {
+    "noResults": "La cerca no ha retornat cap resultat."
+  }
+});
