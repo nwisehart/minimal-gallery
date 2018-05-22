d505fde852e8c3b523c6e95b0159976f79b0773c
Nick <nsenger@esri.com>
2018-05-22 13:27:42 -0700

    feat: Adds ability to filter item types to minimal gallery

--- src/mainApp/MinimalGallery/_reducers/items.ts
+++ src/mainApp/MinimalGallery/_reducers/items.ts
@@ -1,7 +1,8 @@
 import * as ioQuery from "dojo/io-query";
-import { UPDATE_ITEMS, FILTER_ITEMS, HASH_CHANGE } from "../_actions";
+import { UPDATE_ITEMS, FILTER_ITEMS, HASH_CHANGE, SAVE_APP_BASE_RESULT } from "../_actions";
 
 export interface ItemsState {
+    allowedItemTypes: { [type: string]: boolean };
     allItems: __Component.Pojo[];
     filteredItems: __Component.Pojo[];
     displayKey: string;
@@ -9,6 +10,7 @@ export interface ItemsState {
 } 
 
 const initialState: ItemsState = {
+    allowedItemTypes: {},
     allItems: [],
     filteredItems: [],
     displayKey: "",
@@ -17,6 +19,14 @@ const initialState: ItemsState = {
 
 export default (state: ItemsState = initialState, action: { type: string, payload: any }): ItemsState => {
     switch (action.type) {
+        case SAVE_APP_BASE_RESULT:
+            return {
+                ...state,
+                allowedItemTypes: action.payload.config.itemTypes.reduce((r: ItemsState["allowedItemTypes"], c: string) => {
+                    r[c] = true;
+                    return r;
+                }, {})
+            };
         case UPDATE_ITEMS:
             return {
                 ...state,
@@ -29,13 +39,21 @@ export default (state: ItemsState = initialState, action: { type: string, payloa
                 return {
                     ...state,
                     viewerItem: viewerItem ? viewerItem : {},
-                    filteredItems: filterItems(state.allItems, hashParams.query ? hashParams.query : ""),
+                    filteredItems: filterItems(
+                        state.allItems,
+                        hashParams.query ? hashParams.query : "",
+                        state.allowedItemTypes
+                    ),
                     displayKey: Math.random().toString(36).substring(7)
                 };
             }
             return {
                 ...state,
-                filteredItems: filterItems(state.allItems, hashParams.query ? hashParams.query : ""),
+                filteredItems: filterItems(
+                    state.allItems,
+                    hashParams.query ? hashParams.query : "",
+                    state.allowedItemTypes
+                ),
                 displayKey: Math.random().toString(36).substring(7)
             };
         default:
@@ -43,13 +61,15 @@ export default (state: ItemsState = initialState, action: { type: string, payloa
     }
 };
 
-function filterItems(items: __Component.Pojo[], filter: string) {
+function filterItems(items: __Component.Pojo[], filter: string, allowedItemTypes: ItemsState["allowedItemTypes"]) {
     return items.filter((item: __Component.Pojo) => (
-        item.title.toLowerCase().indexOf(filter) !== -1 ||
-        item.type.toLowerCase().indexOf(filter) !== -1 ||
-        item.owner.toLowerCase().indexOf(filter) !== -1 ||
-        (item.tags && item.tags.map((tag: string) => tag.toLowerCase()).indexOf(filter) !== -1) ||
-        (item.description && item.description.indexOf(filter) !== -1) ||
-        (item.snippet && item.snippet.indexOf(filter) !== -1)
+        allowedItemTypes[item.type] && (
+            item.title.toLowerCase().indexOf(filter) !== -1 ||
+            item.type.toLowerCase().indexOf(filter) !== -1 ||
+            item.owner.toLowerCase().indexOf(filter) !== -1 ||
+            (item.tags && item.tags.map((tag: string) => tag.toLowerCase()).indexOf(filter) !== -1) ||
+            (item.description && item.description.indexOf(filter) !== -1) ||
+            (item.snippet && item.snippet.indexOf(filter) !== -1)
+        )
     ));
 }
